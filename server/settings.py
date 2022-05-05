4b015a48920d3955dde40f5e5b96fc42177ab72b
Al Vincent Musa <alvincentvibar.musa@bicol-u.edu.ph>
2022-05-05 19:25:04 +0800

    updated settings.py

--- server/settings.py
+++ server/settings.py
@@ -148,4 +148,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 SESSION_COOKIES_SAMESITE = 'None'
-SESSION_COOKIE_DOMAIN = '..heroku.com' # CONFIG['DOMAIN']
+SESSION_COOKIE_DOMAIN = '..herokuapp.com' # CONFIG['DOMAIN']
